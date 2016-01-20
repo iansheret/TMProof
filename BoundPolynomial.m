325ca8a4c26ebded5b03c3a4a2fba79b135fb1a3
Ian Sheret <ian.sheret@polymathinsight.co.uk>
2016-01-20 16:58:26 +0000

    Update comment header

--- BoundPolynomial.m
+++ BoundPolynomial.m
@@ -1,6 +1,9 @@
 function [l, u] = BoundPolynomial(p, a, b)
 %BOUNDPOLYNOMIAL Bound a polynomial within an interval
 
+% Copyright (c) 2016 Ian Sheret. This project is licensed under the terms
+% of the MIT license. See the LICENSE file for details.
+
 % Get roots of the derivitive
 d = polyder(p);
 r = roots(d);
