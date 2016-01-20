43d91d489649e95ba4b557a7baa4c6d3ee9fb994
Ian Sheret <ian.sheret@polymathinsight.co.uk>
2016-01-20 11:24:01 +0000

    Add interval arithmetic class

---
+++ Interval.m
@@ -0,0 +1,96 @@
+classdef Interval
+%INTERVAL A minimal interval arithmatic class.
+
+% Copyright (c) 2016 Ian Sheret. This project is licensed under the terms
+% of the MIT license. See the LICENSE file for details.
+    
+    properties (SetAccess = immutable)
+        lower
+        upper
+    end
+    
+    methods
+        
+        % Constructor
+        function obj = Interval(a, b)
+            assert(isequal(size(a), [1,1]));
+            assert(isequal(size(b), [1,1]));
+            obj.lower = a;
+            obj.upper = b;
+        end
+        
+        % Promotion
+        function [a, b] = promoteIfNumeric(a, b)
+            if isnumeric(a)
+                a = Interval(a,a);
+            end
+            if nargin>1 && isnumeric(b)
+                b = Interval(b,b);
+            end
+        end
+        
+        % Arithmetic opperators
+        function c = plus(a, b)
+            [a, b] = promoteIfNumeric(a, b);
+            c = Interval(a.lower + b.lower, a.upper + b.upper);
+        end
+        
+        function c = minus(a, b)
+            [a, b] = promoteIfNumeric(a, b);
+            c = Interval(a.lower - b.upper, a.upper - b.lower);
+        end
+        
+        function c = times(a, b)
+            [a, b] = promoteIfNumeric(a, b);
+            v = [...
+                a.lower.*b.lower, a.lower.*b.upper,...
+                a.upper.*b.lower, a.upper.*b.upper];
+            c = Interval(min(v), max(v));   
+        end
+        
+        function c = rdivide(a, b)
+            [a, b] = promoteIfNumeric(a, b);
+            if includesZero(a) && includesZero(b)
+                c = Interval(NaN, NaN); 
+            elseif sign(b.lower)==-1 && sign(b.upper)~=-1
+                c = Interval(-Inf, Inf);    
+            else
+                v = [...
+                    a.lower/b.lower,...
+                    a.lower/b.upper,...
+                    a.upper/b.lower,...
+                    a.upper/b.upper];
+                c = Interval(min(v), max(v)); 
+            end
+        end
+        
+        function c = power(a,b)
+            assert(isnumeric(b));
+            if a.lower < 0
+                assert(mod(b,1)==0);
+            end
+            if b==0
+                c = Interval(1,1);
+            elseif ((mod(b, 2)==1) || (a.lower>=0))
+                c = Interval(a.lower^b, a.upper^b);
+            elseif (a.upper >=0)
+                c = Interval(0, max([a.lower^b, a.upper^b]));
+            else
+                c = Interval(a.upper.^b,a.lower^b);
+            end
+        end
+        
+        % Inclusion of zero, needed for the division opperator.
+        function result = includesZero(a)
+            u = sign(a.upper);
+            l = sign(a.lower);
+            if (u==-1 && l==-1) || (u==1 && l==1)
+                result = false;
+            else
+                result = true;
+            end
+        end
+        
+    end
+    
+end
---
+++ UnitTests/TestInterval.m
@@ -0,0 +1,82 @@
+function tests = TestInterval
+%TESTAUTODIFF Unit tests for the Interval class.
+
+% Copyright (c) 2016 Ian Sheret. This project is licensed under the terms
+% of the MIT license. See the LICENSE file for details.
+
+tests = functiontests(localfunctions);
+
+end
+
+% Constructor
+function TestConstructorGeneratesValidObject(testCase)
+in = Interval(3, 7);
+verifyEqual(testCase, in.lower, 3);
+verifyEqual(testCase, in.upper, 7);
+end
+
+% Arithmetic opperators
+function TestPlusGivesCorrectAnswer(testCase)
+in = Interval(2,5) + Interval(3,7);
+verifyEqual(testCase, in.lower, 5);
+verifyEqual(testCase, in.upper, 12);
+end
+
+function TestMinusGivesCorrectAnswer(testCase)
+in = Interval(2,5) - Interval(3,7);
+verifyEqual(testCase, in.lower,-5);
+verifyEqual(testCase, in.upper, 2);
+end
+
+function TestTimesGivesCorrectAnswer(testCase)
+table = [...
+     2,5,    3, 7,    6,35;...
+    -2,5,    3, 7,  -14,35;...
+    -2,5,    3,-7,  -35,15];
+for i=1:size(table,1)
+    a = Interval(table(i,1), table(i,2));
+    b = Interval(table(i,3), table(i,4));
+    in = a .* b;
+    verifyEqual(testCase, in.lower, table(i,5));
+    verifyEqual(testCase, in.upper, table(i,6));
+end
+end
+
+function TestDivideGivesCorrectAnswerForNonNaNCases(testCase)
+table = [...
+     2,5,    3, 7,    2/7,5/3;...
+     2,5,    0, 7,    2/7,Inf;...
+     2,5,   -3, 0,   -Inf,Inf];
+for i=1:size(table,1)
+    a = Interval(table(i,1), table(i,2));
+    b = Interval(table(i,3), table(i,4));
+    in = a ./ b;
+    verifyEqual(testCase, in.lower, table(i,5));
+    verifyEqual(testCase, in.upper, table(i,6));
+end
+end
+
+function TestDivideGivesCorrectAnswerWhenNaN(testCase)
+a = Interval(-3, 4);
+b = Interval(-2, 5);
+in = a ./ b;
+import matlab.unittest.constraints.HasNaN;
+verifyThat(testCase, in.upper, HasNaN);
+verifyThat(testCase, in.lower, HasNaN);
+end
+
+function TestPowerGivesCorrectAnswer(testCase)
+table = [...
+      2,5,    0,    1,1;...
+      2,5,    1,    2,5;...
+      2,5,    2,    4,25;...
+     -2,5,    2,    0,25;...
+     -2,5,    3,   -8,125];
+for i=1:size(table,1)
+    a = Interval(table(i,1), table(i,2));
+    b = table(i,3);
+    in = a .^ b;
+    verifyEqual(testCase, in.lower, table(i,4));
+    verifyEqual(testCase, in.upper, table(i,5));
+end
+end
