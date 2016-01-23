0d49bd8a1b72851bb7159cace997b576c31ea6d6
Ian Sheret <ian.sheret@polymathinsight.co.uk>
2016-01-23 21:11:17 +0000

    Add routine to invert a function

---
+++ UnitTests/test_invert_function.m
@@ -0,0 +1,40 @@
+function tests = test_invert_function
+%TEST_INVERT_FUNCTION Unit tests for invert_function
+
+% Copyright (c) 2016 Ian Sheret. This project is licensed under the terms
+% of the MIT license. See the LICENSE file for details.
+
+tests = functiontests(localfunctions);
+
+end
+
+function test_non_monotonic_function_is_rejected(test_case)
+f = @(x) -x.*(x-1);
+x_min = 0;
+x_max = 1;
+toll  = 1e-4;
+invert_f = @() invert_function(f, x_min, x_max, toll);
+verifyError(...
+    test_case, invert_f, 'invert_function:input_not_monotonic');
+end
+
+function test_plausable_result_for_valid_input(test_case)
+f = @(x) x.*(6.6 - 2.9.*x);
+
+x_min = 0;
+x_max = 1;
+toll  = 1e-4;
+tic
+g = invert_function(f, x_min, x_max, toll);
+toc
+num_test_points = 1e3;
+x_true = linspace(x_min, x_max, num_test_points);
+y = f(x_true);
+x = zeros(1, num_test_points);
+for i=1:num_test_points
+    x(i) = g(y(i));
+end
+
+verifyEqual(test_case, x, x_true, 'AbsTol', toll);
+
+end
---
+++ invert_function.m
@@ -0,0 +1,63 @@
+function g = invert_function(f, x_min, x_max, toll)
+%INVERT_FUNCTION Invert a function between specified bounds.
+%   G = invert_funcion(F, X_MIN, X_MAX, TOLL) takes a function handle F and
+%   returns its inverse function G. That is, if F defines a function
+%   y = f(x), then G will define x = g(y).
+%
+%   The supplied function must be monotonic between the limits X_MIN and
+%   X_MAX. The output function is only gaurenteed to be valid between
+%   F(X_MIN) and F(X_MAX).
+%
+%   The worst-case accuracy of G is specified by the parameter TOLL.
+
+% Define search parameters
+order = 10;
+max_order = 5;
+
+% Check that the function is monotonic
+df_dx = @(x) get_gradient(f, x);
+if f(x_max) > f(x_min)
+    is_monotonic = prove_in_band(df_dx, x_min, x_max, 0, Inf, order);
+else
+    is_monotonic = prove_in_band(df_dx, x_min, x_max, -Inf, 0, order);
+end
+if ~is_monotonic
+    error('invert_function:input_not_monotonic','Input is not monotonic.');
+end
+
+for n=1:max_order
+    g = @(y) invert_with_fixed_iter(f, y, x_min, x_max, n);
+    residual = @(x) x - g(f(x));
+    is_ok = prove_in_band(residual, x_min, x_max, -toll, toll, order);
+    if is_ok
+        break
+    end
+end
+
+if ~is_ok
+    error(...
+        'invert_function:could_not_find_inversion',...
+        'Failed to invert function');
+end
+
+end
+
+function df_dx = get_gradient(f, x)
+    y     = f(Autodiff(x));
+    df_dx = y.d;
+end
+
+function x = invert_with_fixed_iter(f, y, x_min, x_max, n)
+
+% Get initialisation
+y0 = f(x_min);
+y1 = f(x_max);
+x = x_min + (y-y0).*(x_max-x_min)./(y1-y0);
+
+% Improve with Newton-Raphson
+for i = 1:n
+    fx = f(Autodiff(x, 1));
+    x = x - (fx.x-y)./fx.d;
+end
+
+end
